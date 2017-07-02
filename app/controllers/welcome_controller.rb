e6e9aa327ba285d3cb85169ecca83311f3144ba9
TimingJL <eefozeo@yahoo.com.tw>
2017-07-02 11:46:26 +0800

    modified test data json format

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
   	require 'rest-client'
-  	url = 'https://gist.githubusercontent.com/planetoftheweb/2c2f3b03b72a7f2ae923/raw/d0236fd0a945fdac7acd463f268bd20ebe4d766c/data.json'
+  	url = 'https://raw.githubusercontent.com/TimingJL/nccu-course/master/data/testData.json'
   	raw_content = RestClient.get(url)
   	data = JSON.parse( raw_content )
-  	@name = data["speakers"][0]["name"]
+  	@name = data["911920005"]
   end
 end

1910f11255d9972b20ba91a1ce7b8465a4c89e5d
TimingJL <eefozeo@yahoo.com.tw>
2017-07-03 15:25:27 +0800

    get info name

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
         user.uid = auth.uid
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
-        user.username = auth.info
+        user.username = auth.info.name
       end
   end
 end

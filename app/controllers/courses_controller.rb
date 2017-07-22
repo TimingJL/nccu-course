d2d3d6ab2dc8ada41fb58da57215f5fd457862d4
TimingJL <eefozeo@yahoo.com.tw>
2017-07-22 11:02:25 +0800

    get fb username

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -71,5 +71,5 @@ class CoursesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def issue_params
       params.require(:issue).permit(:title, :content, :courseid, :user, :username, :useremail)
-    end	
+    end
 end

6bb4ecde573191dc80b579703b617e841ef1d212
TimingJL <eefozeo@yahoo.com.tw>
2017-07-23 23:01:51 +0800

    add admin role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,35 @@ class UsersController < ApplicationController
 		@users = User.all
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+	 @user = User.find(params[:id])
+	end
+
+	def update
+	 @user = User.find(params[:id])
+	 if @user.update(user_params)
+	   redirect_to users_path
+	 else
+	   render 'edit'
+	 end
+	end
+
 	def destroy
 		@user = User.find(params[:id])
 
 		if @user.destroy
 		    redirect_to users_path, notice: "User deleted."
 		end
-	end	
+	end
+
+	private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      #params.require(:issue).permit(:title, :content, :courseid, :user, :username, :useremail)
+      params.require(:user).permit(:username, :email, :admin)
+    end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :issues
   resources :comments
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }, path: 'users'
+  devise_for :admins, path: 'admins', as: :authenticated_root
   # get 'welcome/index'
   resources :courses
   resources :users
 
+  match 'users/:id' => 'users#edit', :via => :edit, :as => :admin_edit_user
   match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

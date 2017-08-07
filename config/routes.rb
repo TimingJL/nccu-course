00f47e254d1eb7d9ed6db057226262f093defdd7
TimingJL <eefozeo@yahoo.com.tw>
2017-08-07 13:47:21 +0800

    modify home page

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'courses#index'
+  # root 'courses#index'
+  root 'issues#index'
 end

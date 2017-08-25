136dfcf2448358767c0c80cd20c65672e0133aa3
TimingJL <eefozeo@yahoo.com.tw>
2017-08-25 13:15:28 +0800

    modify type castes in search

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,24 +1,23 @@
 class Course < ApplicationRecord
 	def self.search(search)
 
-	  # where("courseid LIKE ? OR semester LIKE ? OR instructor LIKE ? OR point LIKE ? OR session LIKE ? OR place LIKE ? OR language LIKE ? OR asgeneral LIKE ? OR generalclass LIKE ? OR department LIKE ? OR length LIKE ? OR choose LIKE ? OR coregeneral LIKE ? OR name LIKE ? OR change LIKE ? OR note LIKE ?", 
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%",
-	  # 	"%#{search}%")
-	  where("instructor LIKE ? OR generalclass LIKE ?" ,"%#{search}%", "%#{search}%")
+	  where("courseid = ? OR semester LIKE ? OR instructor LIKE ? OR point LIKE ? OR session LIKE ? OR place LIKE ? OR language LIKE ? OR asgeneral LIKE ? OR generalclass LIKE ? OR department LIKE ? OR length = ? OR choose LIKE ? OR coregeneral LIKE ? OR name LIKE ? OR change LIKE ? OR note LIKE ?", 
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%",
+	  	"%#{search}%")
 
 	end
 end
--- db/migrate/20170825023253_update_courseid_to_index_in_issues.rb
+++ db/migrate/20170825023253_update_courseid_to_index_in_issues.rb
@@ -1,4 +1,6 @@
 class UpdateCourseidToIndexInIssues < ActiveRecord::Migration[5.0]
   def change
+  	change_column :course, :courseid, :string
+  	change_column :course, :length, :string
   end
 end

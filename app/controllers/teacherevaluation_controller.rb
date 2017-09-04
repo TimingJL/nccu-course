ac8345a02842c1647d2840f552ad2f8cbec736a5
TimingJL <eefozeo@gmail.com>
2017-09-04 21:02:30 +0800

    add inst evaluation json file

--- app/controllers/teacherevaluation_controller.rb
+++ app/controllers/teacherevaluation_controller.rb
@@ -1,6 +1,6 @@
 class TeacherevaluationController < ApplicationController
 	def index
-		url = 'https://raw.githubusercontent.com/TimingJL/nccu-course/master/data/instructor-comment-test.json'
+		url = 'https://raw.githubusercontent.com/TimingJL/nccu-course/master/data/inst_evaluation.json'
 		raw_content = RestClient.get(url)
 		@teacherevaluations = JSON.parse( raw_content )
 	end
